a110b7d4226eddd31919144144014f598cfaa145
Pravallikaippagunta <130145825+Pravallikaippagunta@users.noreply.github.com>
2024-08-17 14:55:42 +0530

    Add files via upload

---
+++ DOM Assignment/Event Listener/MouseOver/script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box");
+boxElement.addEventListener("mouseover", function () {
+  alert(
+    "you have entered in Restricted Area (mouse over event is working properly)"
+  );
+});
